be61c3c9680e74525a81383e874df989461e637f
mehedi <mehedi2592000@gmail.com>
2022-05-19 10:52:20 +0600

    dot net core

---
+++ Dot_net_Core/Dot_net_Core/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Dot_net_Core.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
