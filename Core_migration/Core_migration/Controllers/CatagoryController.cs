411c6670045555e068b782da7e65bccf3ca566de
mehedi <mehedi2592000@gmail.com>
2022-06-15 20:32:39 +0600

    tempdata and toastr (notification show korar jonay

--- Core_migration/Core_migration/Controllers/CatagoryController.cs
+++ Core_migration/Core_migration/Controllers/CatagoryController.cs
@@ -20,5 +20,71 @@ namespace Core_migration.Controllers
 
             return View(pass);
         }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();  
+        }
+        [HttpPost]
+        public IActionResult Create(Catagory c)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Catagories.Add(c);
+                context.SaveChanges();
+                TempData["success"] = "created successfully !";
+                return RedirectToAction("Index");
+            }
+            return View(c);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int? id)  //? null value allow na 
+        {
+            if(id==null || id==0)
+            {
+                return NotFound();
+            }
+            var c=context.Catagories.Find(id);
+            if(c==null)
+            {
+                return NotFound();
+            }
+
+            return View(c);
+        }
+        [HttpPost]
+        public IActionResult Edit(Catagory c)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Catagories.Update(c);
+                context.SaveChanges();
+                TempData["success"] = "Edit successfully";
+                return RedirectToAction("Index");
+            }
+            return RedirectToAction("Index");
+        }
+
+        
+        public IActionResult Delete(int? id)  //? null value allow na 
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var c = (from e in context.Catagories
+                     where e.Id == id
+                     select e).FirstOrDefault();
+            if (c == null)
+            {
+                return NotFound();
+            }
+            context.Catagories.Remove(c);
+            context.SaveChanges();
+            TempData["success"] = "Delete successfully";
+            return View("Index");
+        }
     }
 }
